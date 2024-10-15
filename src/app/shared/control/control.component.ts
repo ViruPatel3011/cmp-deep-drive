a07cde76a8bad5a1d41086d90c8ca5af824c99ae
ViruPatel3011 <pviral3011@gmail.com>
2024-10-15 16:45:21 +0530

    @HostListener and @HostBinding

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,7 +15,26 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+
+  // This host property will apply class to all the component place where we use <app-control/>
+  host: {
+    class: 'control', // Alternate approach of  @HostBinding()
+    // Angular teams recommend this approach instead of @HostListener()
+    '(click)': 'onClick()', // Alternate approach of  @HostListener()
+  },
 })
 export class ControlComponent {
+  // This feature just exists for backward compatibility reasons: You should use host:{} instead of this approach
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Click');
+  // }
   label = input.required<string>();
+
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('Click');
+    console.log(this.el);
+  }
 }
